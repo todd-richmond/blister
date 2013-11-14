99f014b4334c3c2aa5c56a384abf51fb9078e308
richmond <richmond>
2013-11-14 23:49:50 +0000

    Xcode 5 and VStudio 13

---
+++ test/Daemonize.cpp
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2001-2013 Todd Richmond
+ *
+ * This file is part of Blister - a light weight, scalable, high performance
+ * C++ server framework.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License. You may
+ * obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "stdapi.h"
+#include "Service.h"
+
+int tmain(int argc, tchar *argv[]) {
+    WatchDaemon wd(argc, argv);
+
+    return wd.execute(argc, argv);
+}
+
