a29b4acf6944264d2c8e88d70a2792c730062e54
Todd Richmond <trichmond@proofpoint.com>
2017-10-23 19:11:17 -0700

    update inet_ntop

--- lib/Socket.cpp
+++ lib/Socket.cpp
@@ -246,21 +246,28 @@ const tstring Sockaddr::str(void) const {
     } else if (family() == AF_INET6) {
 	int i;
 	struct { int base, len; } best, cur;
-	char buf[INET6_ADDRSTRLEN + 1], *p = buf;
-	const int WORDS = sizeof (addr.sa6.sin6_addr) / sizeof (ushort);
+	char tmp[INET6_ADDRSTRLEN + 1], *tp = tmp;
+	const uchar *src = (const uchar *)&addr.sa6.sin6_addr;
+	const int NS_IN6ADDRSZ = 16;
+	const int NS_INT16SZ = 2;
+	const int WORDS = NS_IN6ADDRSZ / NS_INT16SZ;
 	uint words[WORDS];
 
-	ZERO(best);
-	ZERO(cur);
 	ZERO(words);
+	for (i = 0; i < NS_IN6ADDRSZ; i++)
+	    words[i / 2] |= (src[i] << ((1 - (i % 2)) << 3));
 	best.base = -1;
+	best.len = 0;
 	cur.base = -1;
+	cur.len = 0;
 	for (i = 0; i < WORDS; i++) {
 	    if (words[i] == 0) {
-		if (cur.base == -1)
-		    cur.base = i, cur.len = 1;
-		else
+		if (cur.base == -1) {
+		    cur.base = i;
+		    cur.len = 1;
+		} else {
 		    cur.len++;
+		}
 	    } else {
 		if (cur.base != -1) {
 		    if (best.base == -1 || cur.len > best.len)
@@ -276,23 +283,24 @@ const tstring Sockaddr::str(void) const {
 	for (i = 0; i < WORDS; i++) {
 	    if (best.base != -1 && i >= best.base && i < best.base + best.len) {
 		if (i == best.base)
-		    *p++ = ':';
+		    *tp++ = ':';
 		continue;
 	    }
 	    if (i != 0)
-		*p++ = ':';
-	    if (i == 6 && best.base == 0 &&
-		(best.len == 6 || (best.len == 5 && words[5] == 0xffff))) {
-		const uchar *u = (const uchar *)&addr.sa6.sin6_addr + 12;
-		p += sprintf(p, "%u.%u.%u.%u", u[0], u[1], u[2], u[3]);
+		*tp++ = ':';
+	    if (i == 6 && best.base == 0 && (best.len == 6 ||
+		(best.len == 7 && words[7] != 0x0001) ||
+		(best.len == 5 && words[5] == 0xffff))) {
+		src = (const uchar *)&addr.sa6.sin6_addr + 12;
+		tp += sprintf(tp, "%u.%u.%u.%u", src[0], src[1], src[2], src[3]);
 		break;
 	    }
-	    p += sprintf(p, "%x", words[i]);
+	    tp += sprintf(tp, "%x", words[i]);
 	}
 	if (best.base != -1 && (best.base + best.len) == WORDS)
-	    *p++ = ':';
-	*p++ = '\0';
-	return achartotstring(buf);
+	    *tp++ = ':';
+	*tp++ = '\0';
+	return achartotstring(tmp);
     }
     return T("");
 }
