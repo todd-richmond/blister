4f79b9e55389715e0789c2eac2e7021109fc400b
Todd Richmond <trichmond@proofpoint.com>
2021-09-12 22:17:11 -0700

    minor lint

--- lib/Config.h
+++ lib/Config.h
@@ -232,7 +232,9 @@ public:
     }
     bool read(const tchar *file = NULL, const tchar *_pre = NULL, bool append =
 	false);
-    bool write(tostream &os, bool ini) const { return Config::write(os, ini); }
+    bool write(tostream &os, bool inistyle) const {
+	return Config::write(os, inistyle);
+    }
     bool write(void) const { return write(NULL, iniformat()); }
     bool write(const tchar *file, bool ini = false) const;
 
