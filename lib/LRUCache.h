536e074323e9e3c72c90e7c9274c4f84f5227925
Todd Richmond <trichmond@proofpoint.com>
2025-10-10 20:14:46 -0700

    similar optimizations from other classes

--- lib/LRUCache.h
+++ lib/LRUCache.h
@@ -30,19 +30,29 @@ class BLISTER LRUCacheEntry {
 public:
     LRUCacheEntry(const void *d, ulong s): data(NULL), sz(0), msec(0) {
 	const char *p = (const char *)d;
+	lruhash_t h = 0;
 
-	hash = 0;
-	while (s)
-	    hash = hash * 101 + (lruhash_t)p[s--];
+	if (LIKELY(s >= 8 && ((uintptr_t)p & 7) == 0)) {
+	    const uint64_t *p64 = (const uint64_t *)p;
+	    ulong s64 = s >> 3;
+
+	    for (ulong u = 0; u < s64; ++u)
+		h = h * 101 + p64[u];
+	    p += s64 << 3;
+	    s &= 7;
+	}
+	for (ulong u = 0; u < s; ++u)
+	    h = h * 101 + (lruhash_t)p[u];
+	hash = h;
     }
     LRUCacheEntry(const LRUCacheEntry &ce): data(ce.data), sz(ce.sz),
 	hash(ce.hash), msec(ce.msec) {}
 
-    operator bool() const { return data != NULL; }
-    operator lruhash_t() const { return hash; }
+    __forceinline operator bool() const { return data != NULL; }
+    __forceinline operator lruhash_t() const { return hash; }
+    __forceinline msec_t touch(void) const { return msec; }
 
-    msec_t touch(void) const { return msec; }
-    void touch(void) { msec = mticks(); }
+    __forceinline void touch(msec_t now) { msec = now;; }
 
     const void *data;
     ulong sz;
@@ -65,87 +75,100 @@ public:
     explicit LRUCache(ulong sz = LRUCACHE_SIZE, msec_t tm = LRUCACHE_TIME):
 	cursz(0), maxsz(sz), maxtm(tm) {
 	(void)static_cast<LRUCacheEntry *>((C *)0); // enforce base class
+	cache_map.reserve(128);
     }
     ~LRUCache() { clear(); }
 
-    void clear(void) { FastLocker lkr(lock); purge(maxsz); }
+    void clear(void) {
+	FastLocker lkr(lock);
+
+	purge(maxsz, 0);
+    }
     const C get(const void *data, ulong sz) {
 	C entry(data, sz);
 	typename lru_map::const_iterator it;
-	FastLocker lkr(lock);
+	Locker lkr(lock, false);
+	msec_t now;
 
-	purge(0);
-	it = cache_map.find(entry);
-	if (it == cache_map.end()) {
-	    return entry;
+	if (LIKELY(maxtm)) {
+	    now = mticks();
+	    lkr.lock();
+	    purge(0, now);
 	} else {
-	    cache_list.splice(cache_list.begin(), cache_list, it->second);
-	    if (maxtm)
-		it->second->second.touch();
-	    return it->second->second;
+	    lkr.lock();
+	    purge(0, 0);
 	}
+	it = cache_map.find(entry);
+	if (it == cache_map.end())
+	    return entry;
+	cache_list.splice(cache_list.begin(), cache_list, it->second);
+	if (LIKELY(maxtm))
+	    it->second->second.touch(now);
+	return it->second->second;
     }
     bool put(C &entry, const void *data, ulong sz) {
-	FastLocker lkr(lock);
-	typename lru_map::iterator it = cache_map.find(entry);
+	typename lru_map::iterator it;
+	Locker lkr(lock, false);
+	msec_t now;
 
+	if (UNLIKELY(sz > maxsz)) {
+	    entry.data = NULL;
+	    entry.sz = 0;
+	    return false;
+	}
+	now = maxtm ? mticks() : 0;
+	lkr.lock();
+	it = cache_map.find(entry);
 	if (it != cache_map.end()) {
 	    cursz -= it->second->second.sz;
 	    cache_list.erase(it->second);
 	    cache_map.erase(it);
 	}
-	if (sz > maxsz) {
-	    entry.data = NULL;
-	    entry.sz = 0;
-	    purge(0);
-	    return false;
-	}
+	purge(sz, now);
 	entry.data = data;
 	entry.sz = sz;
-	purge(sz);
+	entry.touch(now);
 	cursz += entry.sz;
-	if (maxtm)
-	    entry.touch();
 	cache_list.push_front(lru_kv(entry, entry));
 	cache_map[entry] = cache_list.begin();
 	return true;
     }
     void resize(ulong sz, msec_t tm = 0) {
+	FastLocker lkr(lock);
+
 	maxtm = tm;
 	if (sz < maxsz)
-	    purge(maxsz - sz);
+	    purge(maxsz - sz, maxtm ? mticks() : 0);
 	maxsz = sz;
     }
 
 private:
-    Lock lock;
+    alignas(64) Lock lock;
     lru_list cache_list;
     lru_map cache_map;
-    ulong cursz;
-    ulong maxsz;
+    ulong cursz, maxsz;
     msec_t maxtm;
 
-    void pop(typename lru_list::const_reverse_iterator last) {
+    __forceinline void pop(typename lru_list::const_reverse_iterator last) {
 	cursz -= last->second.sz;
 	delete [] (char *)last->second.data;
 	cache_map.erase(last->first);
 	cache_list.pop_back();
     }
-    void purge(ulong sz) {
+    void purge(ulong sz, msec_t now) {
 	typename lru_list::const_reverse_iterator last;
 
-	if (maxtm) {
-	    msec_t now = mticks();
-
+	if (maxtm && now) {
 	    while ((last = cache_list.rbegin()) != cache_list.rend()) {
-		if (now - last->second.touch() > maxtm)
+		if (LIKELY(now - last->second.touch() > maxtm))
 		    pop(last);
 		else
 		    break;
 	    }
 	}
-	while (cursz + sz > maxsz && !cache_list.empty())
-	    pop(cache_list.rbegin());
+	while (UNLIKELY(cursz + sz > maxsz) && (last = cache_list.rbegin()) !=
+	    cache_list.rend())
+	    pop(last);
     }
 };
 
